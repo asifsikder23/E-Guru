6acc8ea5de03381409d6d1d2ab38281a1eafe8b7
Md. Asif Ullah Sikder <asifsikder23@gmail.com>
2022-10-28 01:46:17 +0600

    data modify

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const app = express()
 const port = 5000
-
+const data = require('./data/data.json')
+const cors = require('cors')
+app.use(cors())
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/courses', (req, res) => {
+    res.send(data)
+  })
+app.get('/course', (req, res) => {
+    res.send(data)
+  })
+ 
+app.get('/course/:id', (req, res) => {
+  const courseId = req.params.id;
+  
+  const course = data.find(single=> single._id==courseId)
+    res.send(course)
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
